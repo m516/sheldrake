91ebef50be657bf27166b83d744795b684fc9d17
M516 <micah112358@gmail.com>
2023-05-17 22:32:47 -0500

    Create example 02, LQR control on a cartpole

--- examples/02_cartpole_lqr/02_cartpole_lqr.cpp
+++ examples/02_cartpole_lqr/02_cartpole_lqr.cpp
@@ -1,37 +1,43 @@
-// This is an official Drake example, written by the Drake community.
-/*
-All components of Drake are licensed under the BSD 3-Clause License
-shown below. Where noted in the source code, some portions may 
-be subject to other permissive, non-viral licenses.
-
-Copyright 2012-2022 Robot Locomotion Group @ CSAIL
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-*/
-
 #include <memory>
-#include <string>
+#include <iostream>
 
 #include <gflags/gflags.h>
 
 #include "drake/common/drake_assert.h"
 #include "drake/geometry/scene_graph.h"
 #include "drake/multibody/parsing/parser.h"
-#include "drake/multibody/plant/multibody_plant.h"
-#include "drake/multibody/tree/prismatic_joint.h"
 #include "drake/multibody/tree/revolute_joint.h"
+#include "drake/multibody/tree/prismatic_joint.h"
 #include "drake/systems/analysis/simulator.h"
+#include "drake/systems/controllers/linear_quadratic_regulator.h"
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/primitives/affine_system.h"
 #include "drake/visualization/visualization_config_functions.h"
 
+using namespace drake;
+using geometry::SceneGraph;
+using multibody::AddMultibodyPlantSceneGraph;
+using multibody::MultibodyPlant;
+using multibody::Parser;
+using multibody::JointActuator;
+using multibody::RevoluteJoint;
+using multibody::PrismaticJoint;
+using systems::Context;
+using systems::InputPort;
+using Eigen::Vector2d;
 
 
 
 
-const std::string cart_pole_udrf = "                                             \
+
+
+namespace cartpole {
+
+
+
+
+
+const std::string cart_pole_sdf = "                                             \
 <?xml version=\"1.0\"?>                                                          \
 <sdf version=\"1.7\">                                                            \
   <model name=\"CartPole\">                                                      \
@@ -123,113 +129,142 @@ const std::string cart_pole_udrf = "                                            
 </sdf>                                                                           \
 ";
 
-using namespace drake;
-using geometry::SceneGraph;
 
-// "multibody" namespace is ambiguous here without "drake::".
-using drake::multibody::AddMultibodyPlantSceneGraph;
-using drake::multibody::MultibodyPlant;
-using drake::multibody::Parser;
-using drake::multibody::PrismaticJoint;
-using drake::multibody::RevoluteJoint;
+
 
 DEFINE_double(target_realtime_rate, 1.0,
               "Desired rate relative to real time.  See documentation for "
               "Simulator::set_target_realtime_rate() for details.");
 
-DEFINE_double(simulation_time, 120.0,
+DEFINE_double(simulation_time, 3.0,
               "Desired duration of the simulation in seconds.");
 
-DEFINE_double(time_step, 0,
-            "If greater than zero, the plant is modeled as a system with "
-            "discrete updates and period equal to this time_step. "
-            "If 0, the plant is modeled as a continuous system.");
+DEFINE_bool(time_stepping, true, "If 'true', the plant is modeled as a "
+    "discrete system with periodic updates. "
+    "If 'false', the plant is modeled as a continuous system.");
 
-
-
-int do_main() {
-  /* 
-  Diagram is a System composed of one or more 
-  constituent Systems, arranged in a directed 
-  graph where the vertices are the constituent 
-  Systems themselves, and the edges connect the 
-  output of one constituent System to the input 
-  of another.  
-  */
-  systems::DiagramBuilder<double> builder;
-
-  // Make and add the cart_pole model.
-  /*
-  Makes a new MultibodyPlant with discrete update 
-  period `time_step` and adds it to a diagram 
-  builder together with the provided SceneGraph 
-  instance, connecting the geometry ports.
-
-  cart_pole is a reference to an empty drake::multibody::MultibodyPlant<double>
-  that will be included in the Diagram created by builder.
-  */
-  auto [cart_pole, scene_graph] =
-      AddMultibodyPlantSceneGraph(&builder, FLAGS_time_step);
-
-  /*
-  Populate the cart_pole with the SDF model of a cart pole
-  */
-  const std::string sdf_url =
-      "package://drake/examples/multibody/cart_pole/cart_pole.sdf";
-  Parser(&cart_pole, &scene_graph).AddModelsFromString(cart_pole_udrf, "sdf");
-
-  // Now the model is complete.
+// This helper method makes an LQR controller to balance cart_pole model
+// specified in the SDF file `file_name`.
+std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController() {
+  // LinearQuadraticRegulator() below requires the controller's model of the
+  // plant to only have a single input port corresponding to the actuation.
+  // Therefore we create a new model that meets this requirement. (a model
+  // created along with a SceneGraph for simulation would also have input ports
+  // to interact with that SceneGraph).
+  MultibodyPlant<double> cart_pole(0.0);
+  Parser parser(&cart_pole);
+  parser.AddModelsFromString(cart_pole_sdf, "sdf");
+  // We are done defining the model.
   cart_pole.Finalize();
 
-  // Now add a visualization to the scene.
-  // This will allow us to see a physical representation
-  // of the Diagram via MeshCat when the simulation begins
-  visualization::AddDefaultVisualization(&builder);
-
-  // That's all there is to this Diagram. Build it now.
-  auto diagram = builder.Build();
-
-  // Create a context for this Diagram
-  std::unique_ptr<systems::Context<double>> diagram_context =
-      diagram->CreateDefaultContext();
-  diagram->SetDefaultContext(diagram_context.get());
-  systems::Context<double>& cart_pole_context =
-      diagram->GetMutableSubsystemContext(cart_pole, diagram_context.get());
-
-  // There is no input actuation in this example for the passive dynamics,
-  // so we'll keep actuation constant at zero.
-  cart_pole.get_actuation_input_port().FixValue(&cart_pole_context, 0.0);
-
-  // Get joints so that we can set initial conditions.
   const PrismaticJoint<double>& cart_slider =
       cart_pole.GetJointByName<PrismaticJoint>("CartSlider");
   const RevoluteJoint<double>& pole_pin =
       cart_pole.GetJointByName<RevoluteJoint>("PolePin");
+  std::unique_ptr<Context<double>> context = cart_pole.CreateDefaultContext();
 
-  // Set initial state.
-  cart_slider.set_translation(&cart_pole_context, 0.0);
-  pole_pin.set_angle(&cart_pole_context, 2);
+  // Set nominal actuation torque to zero.
+  const InputPort<double>& actuation_port = cart_pole.get_actuation_input_port();
+  actuation_port.FixValue(context.get(), 0.0);
+  cart_pole.get_applied_generalized_force_input_port().FixValue(
+      context.get(), Vector2d::Constant(0.0));
 
-  // Simulate the diagram.
-  systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
 
-  // Configure and start the simulator.
-  simulator.set_publish_every_time_step(false);
+  cart_slider.set_translation(context.get(), 0.0);
+  cart_slider.set_translation_rate(context.get(), 0.0);
+  pole_pin.set_angle(context.get(), M_PI);
+  pole_pin.set_angular_rate(context.get(), 0.0);
+
+  // Setup LQR Cost matrices (penalize position error 10x more than velocity
+  // to roughly address difference in units, using sqrt(g/l) as the time
+  // constant.
+  Eigen::Matrix4d Q = Eigen::Matrix4d::Identity();
+  Q(0, 0) = 10;
+  Q(1, 1) = 10;
+  Vector1d R = Vector1d::Constant(1);
+
+  return systems::controllers::LinearQuadraticRegulator(
+      cart_pole, *context, Q, R,
+      Eigen::Matrix<double, 0, 0>::Zero() /* No cross state/control costs */,
+      actuation_port.get_index());
+}
+
+int do_main() {
+  systems::DiagramBuilder<double> builder;
+
+  const double time_step = FLAGS_time_stepping ? 1.0e-3 : 0.0;
+  auto [cart_pole, scene_graph] =
+      AddMultibodyPlantSceneGraph(&builder, time_step);
+
+  // Make and add the cart_pole model.
+  Parser parser(&cart_pole);
+  parser.AddModelsFromString(cart_pole_sdf, "sdf");
+
+  // We are done defining the model.
+  cart_pole.Finalize();
+
+  DRAKE_DEMAND(cart_pole.num_actuators() == 1);
+  DRAKE_DEMAND(cart_pole.num_actuated_dofs() == 1);
+
+  PrismaticJoint<double>& cart_slider =
+      cart_pole.GetMutableJointByName<PrismaticJoint>("CartSlider");
+  RevoluteJoint<double>& pole_pin =
+      cart_pole.GetMutableJointByName<RevoluteJoint>("PolePin");
+
+  // Drake's parser will default the name of the actuator to match the name of
+  // the joint it actuates.
+  const JointActuator<double>& actuator =
+      cart_pole.GetJointActuatorByName("CartSlider");
+  DRAKE_DEMAND(actuator.joint().name() == "CartSlider");
+
+  // For this example the controller's model of the plant exactly matches the
+  // plant to be controlled (in reality there would always be a mismatch).
+  auto controller = builder.AddSystem(
+      MakeBalancingLQRController());
+  controller->set_name("controller");
+  builder.Connect(cart_pole.get_state_output_port(),
+                  controller->get_input_port());
+  builder.Connect(controller->get_output_port(),
+                  cart_pole.get_actuation_input_port());
+
+  // Get the controller design
+  std::cout << "A: " << controller->A() << std::endl;
+  std::cout << "B: " << controller->B() << std::endl;
+  std::cout << "C: " << controller->C() << std::endl;
+  std::cout << "D: " << controller->D() << std::endl;
+
+  visualization::AddDefaultVisualization(&builder);
+  auto diagram = builder.Build();
+
+  systems::Simulator<double> simulator(*diagram);
   simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
-  simulator.Initialize();
-  simulator.AdvanceTo(FLAGS_simulation_time);
+
+  RandomGenerator generator;
+
+  // Setup distribution for random initial conditions.
+  std::normal_distribution<symbolic::Expression> gaussian;
+  cart_slider.set_random_translation_distribution(0);
+  pole_pin.set_random_angle_distribution(M_PI + 0.2*gaussian(generator));
+
+  for (int i = 0; i < 50; i++) {
+    simulator.get_mutable_context().SetTime(0.0);
+    simulator.get_system().SetRandomContext(&simulator.get_mutable_context(),
+                                            &generator);
+
+    simulator.Initialize();
+    simulator.AdvanceTo(FLAGS_simulation_time);
+  }
 
   return 0;
 }
 
-
-
+}  // namespace cartpole
 
 int main(int argc, char* argv[]) {
   gflags::SetUsageMessage(
-      "A simple cart pole demo using Drake's MultibodyPlant,"
-      "with SceneGraph visualization. "
+      "A simple cart pole demo using Drake's MultibodyPlant with "
+      "LQR stabilization. "
       "Launch meldis before running this example.");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  return do_main();
+  return cartpole::do_main();
 }
